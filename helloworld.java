f4626f700357528f8529dee03e73e9223158f5e4
Sarah Dimovski <sarahmdimovski@gmail.com>
2023-09-11 16:18:20 +0200

    Ny tekst

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 class setare{
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("ESBEEENNNNN");
+        System.out.println("hej");
+        System.out.println("ESBEEENNNNN HJÆLP");
     }
 }
 
