0df0071f2c0399903dc5f1e3f999287f0f8ae189
Vittrup02 <tobias2311@gmail.com>
2023-09-11 16:25:14 +0200

    'test'

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,13 @@ class setare{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Setare dater en frømand");
-        
+
+
+
+
+
+
+
+        // Test
     }
 }
