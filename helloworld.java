809ca24749bdc1cb2c252fa1ace6adb6840b7890
Hector K <kristiansen0044@gmail.com>
2023-09-11 16:27:59 +0200

    hej

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class setare{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Setare dater en frømand");
-        
+        System.out.println("Hvad?");
     }
 }
