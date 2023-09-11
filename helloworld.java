4233c9b1ebbc7de0f36bbb7d4a7b374ba58e6a3d
SetareIzadi <setski@protonmail.com>
2023-09-11 16:48:24 +0200

    bruh

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 class setare{
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("Setare dater en frømand");
-        
+        System.out.println("prut");
+
+
     }
 }
