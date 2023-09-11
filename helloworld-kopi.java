7036c5ac15086a21ea5f8bdfa82adaa0de066c0a
s235080 <nikoplund@gmail.com>
2023-09-11 16:45:33 +0200

    wertyui

---
+++ helloworld-kopi.java
@@ -0,0 +1,16 @@
+class setare{
+    public static void main(String[] args) {
+        System.out.println("hello world");;
+        System.out.println("Jeg fatter ikke en skid") ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
