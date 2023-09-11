2619eabe4a3b6aefc5945c3f05cac4ed7d1d21af
Sarah Dimovski <sarahmdimovski@gmail.com>
2023-09-11 16:05:48 +0200

    Ny linje af tekst tilføjet

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
 class setare{
     public static void main(String[] args) {
-        System.out.println("hello world");;
+        System.out.println("hello world");
+        System.out.println("hej");
     }
 }
+git commit
+
