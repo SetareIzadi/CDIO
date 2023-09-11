2c89766c1090a7054bc6fc9dd0e8c86cb6f05f1b
Sarah Dimovski <sarahmdimovski@gmail.com>
2023-09-11 16:38:45 +0200

    jegfatterstadighat

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ class setare{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Setare dater en frømand");
-
+        System.out.println("ESBEN");
 
 
 
