f93556a150c8e0498c0be7d5b435e1c203a503f9
s235080 <144698436+s235080@users.noreply.github.com>
2023-09-18 15:29:09 +0200

    Merge konflikt

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,9 @@ class setare{
     public static void main(String[] args) {
         System.out.println("hello world");;
         System.out.println("Jeg fatter ikke en skid") ;
+
+        Dtbyh 
+        
     }
 }
 
