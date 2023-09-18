ce5c67ae69a2995e73487fe6b21dd3323112a516
SarahDimi <144697325+SarahDimi@users.noreply.github.com>
2023-09-18 15:59:07 +0200

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@ class setare{
         System.out.println("Setare dater en frømand");
         System.out.println("ESBEN");
 
-Sarah er sej
+Dunkle Dennis slays
 jahdjashjdhjasdhasjdhasj 
     
         // Test Test
